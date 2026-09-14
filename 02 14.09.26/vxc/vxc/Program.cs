006a0eed8752a3555d3074fd3577189f6583d6b6
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 11:41:10 +0300

    IfAndElseNesting

---
+++ 02 14.09.26/vxc/vxc/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace vxc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kirjuta enda vanus");
+
+            //peate kasutama if and else lauseid,
+            //et kontrollida, kas kasutaja vanus
+            //on suurem kui 18 või väiksem kui 18
+
+            string userinput = Console.ReadLine();
+            int userage = int.Parse(userinput);
+
+            if (userage > 18)
+            {
+                Console.WriteLine("Sa oled täisealine");
+            }
+            else
+            {
+
+                Console.WriteLine("Sa oled alaealine");
+            }
+        }
+    }
+}
