fd754403fc7b22c9f97cd09d4790d80c6dedee00
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-21 10:00:03 +0300

    21.09.26

---
+++ 01 07.09.26/IfAndElseNesting/IfAndElseNesting/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if and else nestimine iseseisvalt");
+            Console.WriteLine("Teise else if-i sisse panna if ja else");
+
+            Console.WriteLine("Kirjuta oma vanus");
+
+            string userinput = Console.ReadLine();
+            int userage = int.Parse(userinput);
+
+            if (userage < 18)
+            {
+                Console.WriteLine("Oled alaealine");
+            }
+            else if (userage < 20)
+            {
+                Console.WriteLine("Oled oma kahekümnendates");
+            }
+            else if (userage < 30)
+            {
+                Console.WriteLine("Oled oma kolmekümnendates");
+            }
+            else if (userage < 40)
+            {
+                Console.WriteLine("Oled oma neljakümnendates");
+            }
+            else if (userage < 50)
+            {
+                Console.WriteLine("Oled oma viiekümnendates");
+            }
+            else if (userage < 60)
+            {
+                Console.WriteLine("Oled oma kuuekümnendates");
+            }
+            else if (userage < 70)
+            {
+                Console.WriteLine("Oled oma seitmsekümnendates");
+            }
+            else if (userage < 80)
+            {
+                Console.WriteLine("Oled oma kaheksakümnendates");
+            }
+            else if (userage < 90)
+            {
+                Console.WriteLine("Oled oma üheksakümnendates");
+            }
+            else if (userage > 100)
+            {
+                Console.WriteLine("Oled sa kindel, et panid oma vanuse õigesti?");
+            }
+        }
+    }
+}
---
+++ 01 07.09.26/IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string number = Console.ReadLine();
+            int nr = int.Parse(number);
+
+            if (nr == 12)
+            {
+            
+            }
+            else if (nr > 20)
+            {
+                //nestimine e pesastamine
+                if (nr == 25)
+                {
+                    Console.WriteLine("Number 25");
+                }
+                else
+                {
+                    Console.WriteLine("Kõik numbrid peale 25-te");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Kahtlane number");
+            }
+        }
+
+    }
+}
